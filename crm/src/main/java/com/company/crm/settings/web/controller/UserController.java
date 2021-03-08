008119c40df0f4e49b688cd8496f311baeb2e6ba
wl <15911163514@163.com>
2021-03-08 14:45:31 +0800

    123

--- crm/src/main/java/com/company/crm/settings/web/controller/UserController.java
+++ crm/src/main/java/com/company/crm/settings/web/controller/UserController.java
@@ -1,5 +1,12 @@
 package com.company.crm.settings.web.controller;
 
+import com.company.crm.settings.domain.User;
+import com.company.crm.settings.service.UserService;
+import com.company.crm.settings.service.impl.UserServiceImpl;
+import com.company.crm.utils.MD5Util;
+import com.company.crm.utils.PrintJson;
+import com.company.crm.utils.ServiceFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,12 +19,35 @@ public class UserController extends HttpServlet {
         System.out.println("进入到用户控制器");
         System.out.println("ContextPath:"+request.getContextPath());
         String path = request.getServletPath();
-
-        if("/settings/user/xxx.do".equals(path)){
-
+        if("/settings/user/login.do".equals(path)){
+            login(request,response);
         }else if("/settings/user/xxx.do".equals(path)){
 
         }
 
+    }
+
+    private void login(HttpServletRequest request, HttpServletResponse response) {
+
+        System.out.println("进入验证登录操作");
+        String loginAct = request.getParameter("loginAct");
+        String loginPwd = request.getParameter("loginPwd");
+
+        loginPwd = MD5Util.getMD5(loginPwd);
+        String ip = request.getRemoteAddr();
+        System.out.println("ip地址："+ip);
+        UserService us = (UserService) ServiceFactory.getService(new UserServiceImpl());
+
+        try{
+//            User user = us.login(loginAct,loginPwd,ip);
+//            request.getSession().setAttribute("user",user);
+//            PrintJson.printJsonFlag(response,true);
+
+        }catch(Exception e){
+            e.printStackTrace();
+            String msg = e.getMessage();
+            //P63 17分钟
+
+        }
     }
 }
---
+++ crm/src/main/webapp/ajax.jsp
@@ -0,0 +1,29 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zytwl
+  Date: 2021/3/8
+  Time: 11:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<script>
+    $.ajax({
+        url:"",
+        data:{
+
+        },
+        type:"",
+        dataType:"json",
+        success:function(data){
+
+        }
+    })
+</script>
+<body>
+
+</body>
+</html>
